636137bd0808fd306d46d8f9cabcdba36ee534f4
rshorey <rshorey@uncc.edu>
2020-06-02 23:56:45 -0400

    first commit

---
+++ src/main/java/com/itcs6112/oas/OnlineAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itcs6112.oas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAppointmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itcs6112/oas/OnlineAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcs6112.oas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
